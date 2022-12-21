663a8f8fda99d23505bc3f747f341bce9c15f410
avc285 <avc285@gmail.com>
2022-12-21 10:20:51 +0000

    draft nav and logo components

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Nav() {
+    return (
+        <div className="Nav">
+            <ul>
+                <li>Playlists</li>
+                <li>Top Locations</li>
+                <li>Top Cuisines</li>
+                <li>Random playlist</li>
+            </ul>
+        </div>
+    );
+}
