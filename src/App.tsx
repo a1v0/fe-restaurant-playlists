242964b6155da7e93c1adda77a0cd814f1adec66
Cnmoosavinia <cyrusmoosavinia@gmail.com>
2022-12-21 09:12:36 +0000

    browser/routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Playlists from './components/Playlists';
 
 function App() {
   return (
     <div className="App">
-      <h1>Restaurant Playlists</h1>
+      <Header />
+      <Routes>
+      <Route path="/playlists" element={<Playlists />}></Route>  
+      </Routes>
+      <Footer />
     </div>
   );
 }
