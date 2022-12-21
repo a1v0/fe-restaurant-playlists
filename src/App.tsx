27919b2a8174eca700880846f77e53d852879a6d
avc285 <avc285@gmail.com>
2022-12-21 10:21:57 +0000

    render header in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Playlists from './components/Playlists';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Playlists from "./components/Playlists";
+import LogoAndUser from "./components/LogoAndUser";
+import Nav from "./components/Nav";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-      <Route path="/playlists" element={<Playlists />}></Route>  
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <LogoAndUser />
+            <Nav />
+            <Routes>
+                <Route path="/playlists" element={<Playlists />}></Route>
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
