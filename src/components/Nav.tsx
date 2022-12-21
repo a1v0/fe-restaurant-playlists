bb71b6a109230cf988f1d97a04558b03f435a6b6
avc285 <avc285@gmail.com>
2022-12-21 11:36:13 +0000

    minor style mods

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
         <div className="Nav">
             <ul>
-                <li>Playlists</li>
+                <li>
+                    <Link to="/playlists">Playlists</Link>
+                </li>
                 <li>Top Locations</li>
                 <li>Top Cuisines</li>
                 <li>Random playlist</li>
