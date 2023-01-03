6ee55868afafa38c40881270bdcc6eea2dbe01a3
avc285 <avc285@gmail.com>
2023-01-03 23:42:38 +0000

    create and style top section for mobile

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Home() {
-    return <div className="Home"></div>;
+    return (
+        <main className="Home">
+            <div className="welcome">
+                <section>
+                    <h1>Restaurant Playlists</h1>
+                    <p>
+                        Food is delicious and sharing is caring. Why not share
+                        delicious food with others?
+                    </p>
+                    <p>
+                        Here at Restaurant Playlists, we make that simple. You
+                        can create and share "playlists" of your favourite
+                        restaurants.
+                    </p>
+                </section>
+            </div>
+        </main>
+    );
 }
