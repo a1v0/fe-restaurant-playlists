583af0c5115b4b344c72f9f6122c1186ed742e00
avc285 <avc285@gmail.com>
2023-01-03 15:33:56 +0000

    add place properties to request

--- src/google-api.ts
+++ src/google-api.ts
@@ -1,29 +1,34 @@
 import { Loader } from "@googlemaps/js-api-loader";
 
 const loader = new Loader({
+    // *** TO DO *** potentially try to hide API key in .ENV file
     apiKey: "AIzaSyC8YX-J9zTpwgUfrZSBlvuImrn2mj99X8c",
     version: "weekly",
     libraries: ["places"]
 });
 
-export const getPlaceDetails = (): void => {
+export const getPlaceDetails = (placeId: string): void => {
     loader.load().then(() => {
         const map = new google.maps.Map(
-            document.getElementById("map") as HTMLElement,
-            {
-                center: { lat: -33.866, lng: 151.196 },
-                zoom: 15
-            }
+            document.getElementById("map") as HTMLElement
         );
         const request = {
-            placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-            fields: ["name", "place_id", "formatted_address"]
+            placeId,
+            fields: [
+                "name",
+                "formatted_address",
+                "photo",
+                "url",
+                "website"
+                // opening_hours, photos (possibly photo is wrong)
+                // price_level, rating, reviews are useful but more expensive fields
+                // *** TO DO *** add a subtle link to "url" field, because Google requests this
+            ]
         };
         const service = new google.maps.places.PlacesService(map);
 
         service.getDetails(request, (place, status) => {
             console.log(place);
-            console.log(status);
         });
     });
 };
