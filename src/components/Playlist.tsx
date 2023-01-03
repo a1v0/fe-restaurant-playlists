4aeb62e5b7a70846acc284b183c831c510c5a979
yos244 <yos_24@hotmail.com>
2023-01-03 10:42:52 +0000

    create and style playlist component

---
+++ src/components/Playlist.tsx
@@ -0,0 +1,35 @@
+
+
+function Playlist() {
+return (
+    <main className="Playlist">
+        <h2>Playlist Name</h2>
+        <div className="playlist-details">
+        <p>Playlist Owner</p>
+        <p>Playlist Tags</p>
+        </div>
+        <h1>Playlist Picture</h1>
+        <p>Playlist Description</p>
+        <ul>
+            <li>
+                <h3>Restaurant Name</h3>
+                <p>Restaurant Address</p>
+            </li>
+            <li>
+                <h3>Restaurant Name</h3>
+                <p>Restaurant Address</p>
+            </li>
+            <li>
+                <h3>Restaurant Name</h3>
+                <p>Restaurant Address</p>
+            </li>
+            <li>
+                <h3>Restaurant Name</h3>
+                <p>Restaurant Address</p>
+            </li>
+        </ul>
+    </main>
+)  
+}
+
+export default Playlist;
