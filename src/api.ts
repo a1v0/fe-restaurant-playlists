8c086362cd2df6cf476e6acd3c48b21dc17d34da
avc285 <avc285@gmail.com>
2023-01-03 12:08:58 +0000

    add getPlaylistById API request

--- src/api.ts
+++ src/api.ts
@@ -2,12 +2,17 @@ import axios from "axios";
 
 const apiConnection = axios.create({
     baseURL: "http://127.0.0.1:5000/api"
-})
+});
 
 export const getPlaylists = () => {
-    return apiConnection
-    .get("/playlists", {}) 
-    .then((res) => {
-        return res.data.playlists
-    })
-}
+    return apiConnection.get("/playlists").then((res) => {
+        return res.data.playlists;
+    });
+};
+
+export const getPlaylistById = (playlist_id: number) => {
+    // ***TO DO*** error handling for invalid playlist_id
+    return apiConnection.get(`/playlists/${playlist_id}`).then((res) => {
+        return res.data.playlist;
+    });
+};
