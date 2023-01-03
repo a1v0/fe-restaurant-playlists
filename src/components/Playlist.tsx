f0786207b9568647c139a67a2e8f7882bac8df24
avc285 <avc285@gmail.com>
2023-01-03 12:09:14 +0000

    add props to Playlist

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,35 +1,35 @@
+import { getPlaylistById } from "../api";
 
-
-function Playlist() {
-return (
-    <main className="Playlist">
-        <h2>Playlist Name</h2>
-        <div className="playlist-details">
-        <p>Playlist Owner</p>
-        <p>Playlist Tags</p>
-        </div>
-        <h1>Playlist Picture</h1>
-        <p>Playlist Description</p>
-        <ul>
-            <li>
-                <h3>Restaurant Name</h3>
-                <p>Restaurant Address</p>
-            </li>
-            <li>
-                <h3>Restaurant Name</h3>
-                <p>Restaurant Address</p>
-            </li>
-            <li>
-                <h3>Restaurant Name</h3>
-                <p>Restaurant Address</p>
-            </li>
-            <li>
-                <h3>Restaurant Name</h3>
-                <p>Restaurant Address</p>
-            </li>
-        </ul>
-    </main>
-)  
+function Playlist(props: { playlist_id: string }) {
+    return (
+        <main className="Playlist">
+            <h2>Playlist Name</h2>
+            <div className="playlist-details">
+                <p>Playlist Owner</p>
+                <p>Playlist Tags</p>
+            </div>
+            <h1>Playlist Picture</h1>
+            <p>Playlist Description</p>
+            <ul>
+                <li>
+                    <h3>Restaurant Name</h3>
+                    <p>Restaurant Address</p>
+                </li>
+                <li>
+                    <h3>Restaurant Name</h3>
+                    <p>Restaurant Address</p>
+                </li>
+                <li>
+                    <h3>Restaurant Name</h3>
+                    <p>Restaurant Address</p>
+                </li>
+                <li>
+                    <h3>Restaurant Name</h3>
+                    <p>Restaurant Address</p>
+                </li>
+            </ul>
+        </main>
+    );
 }
 
 export default Playlist;
