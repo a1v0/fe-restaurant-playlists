81f6bed4c383b1deb123866ca322bc66908d5edc
Musab Hussain <moshkhussain@gmail.com>
2023-01-04 22:18:25 +0000

    VoteStars button rendering - saving progress

--- src/components/VoteStars.tsx
+++ src/components/VoteStars.tsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../VoteStars.css";
 
 function VoteStars() {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    console.log(rating)
+
     return (
-        <div className="Votes">
-            <Link className="star" to="">
-                ⭐
-            </Link>
-            <Link className="star" to="">
-                ⭐
-            </Link>
-            <Link className="star" to="">
-                ⭐
-            </Link>
-            <Link className="star" to="">
-                ⭐
-            </Link>
-            <Link className="star" to="">
-                ⭐
-            </Link>
+        <div className="star-rating">
+            {[...Array(5)].map((star, index) => {
+                index += 1;
+                return (
+                    <button
+                        type="button"
+                        key={index}
+                        className={index <= (hover || rating) ? "on" : "off"}
+                        onClick={() => setRating(index)}
+                        onMouseEnter={() => setHover(index)}
+                        onMouseLeave={() => setHover(rating)}
+                        onDoubleClick={() => {
+                            setRating(0);
+                            setHover(0);
+                        }}
+                    >
+                        <span className="star">&#9733;</span>
+                    </button>
+                );
+            })}
         </div>
     );
 }
