aacac7a9010fd1adc752fb058192e005d13284a2
avc285 <avc285@gmail.com>
2023-01-04 18:33:38 +0000

    refactor login/logout buttons to be Links

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
-  return (
-    <button
-      className="btn btn-danger btn-block"
-      onClick={() =>
-        logout({
-          returnTo: window.location.origin,
-        })
-      }
-    >
-      Log Out
-    </button>
-  );
+    const { logout } = useAuth0();
+    return (
+        <Link
+            to=""
+            onClick={() =>
+                logout({
+                    returnTo: window.location.origin,
+                })
+            }
+        >
+            Log Out
+        </Link>
+    );
 };
 
 export default LogoutButton;
