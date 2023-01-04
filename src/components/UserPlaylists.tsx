03570d399b5b4cdd18fb3eb8b1ec7328cc5f4f17
Musab Hussain <moshkhussain@gmail.com>
2023-01-04 23:18:50 +0000

    No VoteStars to show in UserPlaylists component

--- src/components/UserPlaylists.tsx
+++ src/components/UserPlaylists.tsx
@@ -78,23 +78,16 @@ function UserPlaylists() {
                                         ) : null}
                                     </p>
                                     <div className="review-data">
-                                        <p>{playlist.vote_count}</p>
-                                        <Link className="star" to="">
-                                            ⭐
-                                        </Link>
-                                        <Link className="star" to="">
-                                            ⭐
-                                        </Link>
-                                        <Link className="star" to="">
-                                            ⭐
-                                        </Link>
-                                        <Link className="star" to="">
-                                            ⭐
-                                        </Link>
-                                        <Link className="star" to="">
-                                            ⭐
-                                        </Link>
-                                        <p>Reviews: {playlist.total_votes}</p>
+                                        {playlist.vote_count ? (
+                                            <p>
+                                                Average Review{" "}
+                                                {playlist.vote_count}
+                                            </p>
+                                        ) : null}
+                                        <p>
+                                            Total Reviews:{" "}
+                                            {playlist.total_votes}
+                                        </p>
                                     </div>
                                     <div>
                                         {user?.name === playlist.nickname ? (
