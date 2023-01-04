3e11f8dec2ddf43abf541456d36268a785c0708b
avc285 <avc285@gmail.com>
2023-01-04 14:27:23 +0000

    create new playlist form

--- src/components/CreatePlaylist.tsx
+++ src/components/CreatePlaylist.tsx
@@ -1,14 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { initAutocomplete } from "../google-api";
 
 export default function CreatePlaylist() {
+    const initialState: string[] = [];
+    const [restaurantsToAdd, setRestaurantsToAdd] = useState(initialState);
+    const [restaurantAdded, setRestaurantAdded] = useState(false);
+
     useEffect(() => {
         initAutocomplete();
-    }, []);
+        console.log(restaurantsToAdd);
+    }, [restaurantsToAdd]);
 
     const handleAddToPlaylist = () => {
         initAutocomplete().then(() => {
-            console.log(JSON.parse(window.sessionStorage.googlePlaceId));
+            setRestaurantsToAdd([
+                ...restaurantsToAdd,
+                JSON.parse(window.sessionStorage.googlePlaceId),
+            ]);
+            setRestaurantAdded(true);
         });
     };
 
@@ -18,8 +27,33 @@ export default function CreatePlaylist() {
                 event.preventDefault();
             }}
         >
-            <input id="autocomplete" type="text" placeholder="hello"></input>
-            <button onClick={handleAddToPlaylist}>add to playlist</button>
+            <label>
+                Playlist name
+                <input type="text" id="playlist-name"></input>
+            </label>
+            <label>
+                Description
+                <input type="text" id="playlist-description"></input>
+            </label>
+            <label>
+                location
+                <input type="text" id="playlist-location"></input>
+            </label>
+            <label>
+                cuisine
+                <input type="text" id="playlist-cuisine"></input>
+            </label>
+            <label>
+                Choose restaurants for your playlist{" "}
+                <input
+                    id="autocomplete"
+                    type="text"
+                    placeholder="type restaurant name"
+                ></input>
+                <button onClick={handleAddToPlaylist}>add restaurant</button>
+                {restaurantAdded ? <p>restaurant added successfully</p> : null}
+            </label>
+            <button type="submit">Create Playlist</button>
         </form>
     );
 }
